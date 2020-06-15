8937eb65d9c9940fb5e34d500cc9eff0be005365
maximog <maximo.gijon@gmail.com>
2020-06-14 23:48:50 -0300

    Use json-api-normalizer and redux-object libraries to transform API call response data in Countries component

--- src/pages/Countries/index.js
+++ src/pages/Countries/index.js
@@ -1,4 +1,7 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from 'react';
+
+import normalize from 'json-api-normalizer';
+import build from 'redux-object';
 
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -8,52 +11,60 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
-import { getFrom } from "../../api/api"
+import { getFrom } from '../../api/api';
 
 import Search from './Search';
 
 export default function Countries() {
-  let [countries, setCountries] = useState([])
+  let [countries, setCountries] = useState([]);
 
-  let [searchText, setSearchText] = useState("")
+  let [searchText, setSearchText] = useState('');
 
   useEffect(() => {
     getFrom(`counties/?state=&search=${searchText}`)
       .then((response) => {
-        console.log(response)
-        setCountries(response.data.data)
+        console.log(response);
+        const normalizedDataObject = normalize(response.data);
+        const countiesArray = build(normalizedDataObject, 'county');
+        setCountries(countiesArray ? countiesArray : []);
       })
       .catch((error) => {
-        console.log(error)
-      })
-  }, [searchText])
+        console.log(error);
+      });
+  }, [searchText]);
 
   return (
-    <div style={{padding: '20px'}}>
+    <div style={{ padding: '20px' }}>
       <h2 data-testid="title">Counties</h2>
-      <div style={{marginBottom: '20px'}}>
+      <div style={{ marginBottom: '20px' }}>
         <Search handleChange={setSearchText} />
       </div>
-      {searchText ? <div data-testid="table"><TableContainer component={Paper}>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>State</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {countries.map((country, index) => (
-              <TableRow key={index}>
-                <TableCell component="th" scope="row">
-                  {country.attributes.name}
-                </TableCell>
-            <TableCell>{country.attributes.state_name}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer></div>  :   <p>Enter search text</p>}
+      {searchText ? (
+        <div data-testid="table">
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>State</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {countries.map((country) => (
+                  <TableRow key={country.id}>
+                    <TableCell component="th" scope="row">
+                      {country.name}
+                    </TableCell>
+                    <TableCell>{country.stateName}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+      ) : (
+        <p>Enter search text</p>
+      )}
     </div>
-  )
+  );
 }
