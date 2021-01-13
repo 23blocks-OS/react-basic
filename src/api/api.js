ee8629a98ddd708da32823befea890377f10ac85
Maximo Gijon <maximo.gijon@gmail.com>
2021-01-13 19:52:28 -0300

    copy changes from cicero

--- src/api/api.js
+++ src/api/api.js
@@ -13,7 +13,25 @@ instance.interceptors.response.use(
     return Promise.reject(formatErrorResponse(error.response));
   }
 );
-// Error handling for axios response
+
+instance.interceptors.request.use((request) => {
+  const authData = getAuthData();
+  if (authData.companyToken) {
+    request.headers.common['company-token'] = authData.companyToken;
+    request.headers.common['access-token'] = authData.accessToken;
+    request.headers.common['client'] = authData.client;
+    request.headers.common['expiry'] = authData.expiry;
+    request.headers.common['token-type'] = authData.tokenType;
+    request.headers.common['uid'] = authData.uid;
+    request.headers.common['appid'] = process.env.REACT_APP_API_KEY;
+    // console.log('set headers');
+  } else {
+    request.headers.common['appid'] = process.env.REACT_APP_API_KEY;
+    // console.log('set api key');
+  }
+  return request;
+});
+
 function formatErrorResponse(error) {
   let details = get(error, 'data.errors[0].detail', 'Try again');
 
@@ -24,20 +42,20 @@ function formatErrorResponse(error) {
   return { error, errorDetail: details };
 }
 
-// Add 'APPID' header to axios requests
 export function setApiKey() {
-  console.log('Setting api key', process.env.REACT_APP_API_KEY);
+  // console.log('Setting api key', process.env.REACT_APP_API_KEY);
   instance.defaults.headers.common['APPID'] = process.env.REACT_APP_API_KEY;
 }
 
 // Add 'company-Token' header to axios requests
-export function setCompanyToken() {
-  console.log('Setting company token', process.env.REACT_APP_COMPANY_TOKEN);
-  instance.defaults.headers.common['company-token'] =
-    process.env.REACT_APP_COMPANY_TOKEN;
+export function setCompanyToken(companyToken) {
+  instance.defaults.headers.common['company-token'] = companyToken;
+  // console.log(
+  //   'Setting gateway company token',
+  //   instance.defaults.headers.common['company-token']
+  // );
 }
 
-// Send form data to API POST request
 export function sendFormData(url, data) {
   return instance({
     method: 'post',
@@ -47,7 +65,6 @@ export function sendFormData(url, data) {
   });
 }
 
-// Standard GET request from API
 export function getFrom(url) {
   return instance({
     method: 'get',
@@ -55,7 +72,20 @@ export function getFrom(url) {
   });
 }
 
-// Convert object to form data format to send in POST requests
+export function validateToken() {
+  return instance({
+    method: 'get',
+    url: '/auth/validate_token',
+  });
+}
+
+export function signOut() {
+  return instance({
+    method: 'delete',
+    url: '/auth/sign_out',
+  });
+}
+
 function objectToFormData(object) {
   var data = new FormData();
 
@@ -65,3 +95,46 @@ function objectToFormData(object) {
 
   return data;
 }
+
+export const getAuthData = () => {
+  return {
+    companyToken: localStorage.getItem('company-token'),
+    accessToken: localStorage.getItem('access-token'),
+    client: localStorage.getItem('client'),
+    expiry: localStorage.getItem('expiry'),
+    tokenType: localStorage.getItem('token-type'),
+    uid: localStorage.getItem('uid'),
+    appid: process.env.REACT_APP_API_KEY,
+  };
+};
+
+export const setAuthData = (authApiResponse) => {
+  const headers = authApiResponse.headers;
+  const authData = {
+    companyToken: headers['company-token'],
+    accessToken: headers['access-token'],
+    client: headers['client'],
+    expiry: headers['expiry'],
+    tokenType: headers['token-type'],
+    uid: headers['uid'],
+    appid: process.env.REACT_APP_API_KEY,
+  };
+
+  localStorage.setItem('company-token', authData.companyToken);
+  localStorage.setItem('access-token', authData.accessToken);
+  localStorage.setItem('client', authData.client);
+  localStorage.setItem('expiry', authData.expiry);
+  localStorage.setItem('token-type', authData.tokenType);
+  localStorage.setItem('uid', authData.uid);
+  localStorage.setItem('appid', process.env.REACT_APP_API_KEY);
+};
+
+export const setHeaders = (authData) => {
+  instance.defaults.headers.common['company-token'] = authData.companyToken;
+  instance.defaults.headers.common['access-token'] = authData.accessToken;
+  instance.defaults.headers.common['client'] = authData.client;
+  instance.defaults.headers.common['expiry'] = authData.expiry;
+  instance.defaults.headers.common['token-type'] = authData.tokenType;
+  instance.defaults.headers.common['uid'] = authData.uid;
+  instance.defaults.headers.common['appid'] = process.env.REACT_APP_API_KEY;
+};
--- src/api/productsApi.js
+++ src/api/productsApi.js
@@ -13,6 +13,19 @@ productsInstance.interceptors.response.use(
     return Promise.reject(formatErrorResponse(error.response));
   }
 );
+
+productsInstance.interceptors.request.use((request) => {
+  const authData = getAuthData();
+  if (authData.companyToken) {
+    request.headers.common['company-token'] = authData.companyToken;
+    request.headers.common['appid'] = process.env.REACT_APP_API_KEY;
+    // console.log('set product headers');
+  } else {
+    request.headers.common['appid'] = process.env.REACT_APP_API_KEY;
+    // console.log('set product api key');
+  }
+  return request;
+});
 // Error handling for axios response
 function formatErrorResponse(error) {
   let details = get(error, 'data.errors[0].detail', 'Try again');
@@ -26,15 +39,17 @@ function formatErrorResponse(error) {
 
 // Add 'APPID' header to axios requests
 export function setApiKey() {
-  console.log('Setting products api key', process.env.REACT_APP_RESTTOGO_API_KEY);
+  // console.log('Setting products api key', process.env.REACT_APP_API_KEY);
   productsInstance.defaults.headers.common['APPID'] = process.env.REACT_APP_API_KEY;
 }
 
 // Add 'company-Token' header to axios requests
-export function setCompanyToken() {
-  console.log('Setting company token', process.env.REACT_APP_COMPANY_TOKEN);
-  productsInstance.defaults.headers.common['company-token'] =
-    process.env.REACT_APP_COMPANY_TOKEN;
+export function setCompanyToken(companyToken) {
+  productsInstance.defaults.headers.common['company-token'] = companyToken;
+  // console.log(
+  //   'Setting products company token',
+  //   productsInstance.defaults.headers.common['company-token']
+  // );
 }
 
 // Send form data to API POST request
@@ -55,6 +70,39 @@ export function getFrom(url) {
   });
 }
 
+// Send form data to API POST request
+export function sendPutRequest(url, data) {
+  return productsInstance({
+    method: 'put',
+    url,
+    data: objectToFormData(data),
+    headers: { 'Content-Type': 'multipart/form-data' },
+  });
+}
+
+export function updateCartItemQty(userId, item, qty) {
+  return productsInstance({
+    method: 'put',
+    url: '/carts/',
+    data: objectToFormData({
+      'cart[user_unique_id]': userId,
+      'cart[notes]': '',
+      'product[sku]': item.sku || item.productSku,
+      'product[notes]': '',
+      'product[quantity]': qty,
+    }),
+    headers: { 'Content-Type': 'multipart/form-data' },
+  });
+}
+
+// GET Cart content
+export function getCart(userId) {
+  return productsInstance({
+    method: 'get',
+    url: '/carts/' + userId,
+  });
+}
+
 // Convert object to form data format to send in POST requests
 function objectToFormData(object) {
   var data = new FormData();
@@ -65,3 +113,15 @@ function objectToFormData(object) {
 
   return data;
 }
+
+export const getAuthData = () => {
+  return {
+    companyToken: localStorage.getItem('company-token'),
+    accessToken: localStorage.getItem('access-token'),
+    client: localStorage.getItem('client'),
+    expiry: localStorage.getItem('expiry'),
+    tokenType: localStorage.getItem('token-type'),
+    uid: localStorage.getItem('uid'),
+    appid: process.env.REACT_APP_API_KEY,
+  };
+};
