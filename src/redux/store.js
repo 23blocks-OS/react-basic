3695b7c55fc0fb88dd3daab731f8a125ae6656ea
Maximo Gijon <maximo.gijon@gmail.com>
2021-01-13 19:52:07 -0300

    create store

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import rootReducer from './root-reducer';
+import thunkMiddleware from 'redux-thunk';
+
+const middlewares = [thunkMiddleware, logger];
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export default store;
